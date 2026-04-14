1afa6290a5efc3724c264e028f24bb6e5f45b844
Ryan Shaik <ryanshaik16@gmail.com>
2026-04-14 10:56:54 +0600

    Fix Stripe initialization for build time

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,11 @@
 import Stripe from 'stripe';
 
 if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not defined');
+  console.warn('STRIPE_SECRET_KEY is not defined');
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-12-18.acacia',
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder', {
+  apiVersion: '2026-03-25.dahlia',
   typescript: true,
 });
 
